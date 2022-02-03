e6e3f62c4bf0dc54574095084678c04e25b4abcf
Zachary Miller <91511336+mrzcmiller@users.noreply.github.com>
2022-02-03 14:54:16 -0500

    UpdateToProblem1
    Updated to return an int[] instead of Object array after research regarding performance/best practices. Also, following a re-read of the requirements, additional functionality was added to eliminate the possibility of duplicate values.

--- Problem1_31Jan2022.java
+++ Problem1_31Jan2022.java
@@ -13,25 +13,66 @@ public class Problem1_31Jan2022 {
 //	Create a function that takes an array of non-negative integers and strings and returns a new array without the strings.
 	
 	/* 
-	 * The getResult method processes an Object array provided as an argument and returns an Object[] of Integer objects
-	 * A List of Object types is created to accommodate an uncertain number of Integer objects in any given array
+	 * The getResult method processes an Object array provided as an argument and returns an int[]
+	 * A List of Integer types is created to accommodate an uncertain number of Integer objects in any given array
 	 * A for loop then iterates through the array checking to see if the object is an instance of the Integer class and
-	 * if it is, adds it to the List. The list is then converted to and stored in an Object[] and set to null for garbage 
-	 * collection.
-	 * The Object[] is then returned to the main method
+	 * if it is, navigates a second for loop to see if that value alread exits in the List. If the second for loop 
+	 * completes without finding an equal value, it adds the current value to the List. If a duplicate is found, a 
+	 * 'duplicate' boolean is set to true and the item is not added to the list. Once the loops complete, the initial array
+	 * is set to null, and integer array is created for the size of the list, a for loop iterates through the list and stores
+	 * the integer value at each position into the int[] corresponding position. The List is set to null for garbage 
+	 * collection and the int[] is returned to the main method.
 	 */
-	public static Object[] getResult(Object[] array) {
-		List<Object> arrayItems = new ArrayList<Object>(); // Create a List with type Object to stores Integer objects
+	public static int[] getResult(Object[] array) {
+		List<Integer> arrayItems = new ArrayList<Integer>(); // Create a List with type Object to stores Integer objects
+		boolean duplicate = false; // Used to avoid ConcurrentModificationException by delaying List modification until after the loop completes
 		
 		for (Object item : array) { // Loop through the array provided as an argument
-			if(item instanceof Integer) { // Check to see if the current object is an instance of the Integer class
-				arrayItems.add(item); // If the object is an Integer object, add it to the arrayItems List
+			
+			/* 
+			 * Check if array is empty and if so, add the current item to the arrayItems
+			 * This is necessary to avoid the initially null list from bypassing the inner for loop which 
+			 * traverses the arrayItems List
+			 */
+			if ( arrayItems.isEmpty() ) {
+				arrayItems.add( (Integer) item ); // add Integer object to arrayItems ArrayList
+				continue;
 			}
+			
+			if(item instanceof Integer && item != null) { // Check to see if the current object is an instance of the Integer class
+				
+				for ( Integer integer : arrayItems ) { // Iterate through arrayItems to check if the current value is already present
+					
+					if ( integer.intValue() == Integer.valueOf((Integer) item) ) {
+						duplicate = true; // If value is already present, set duplicate property to true
+						break; // Exit the current loop 
+					}
+					
+					else {
+						duplicate = false; // Set the duplicate property to false and continue to the next integer
+						continue;
+					}
+					
+				}
+				
+				if (!duplicate) {
+					arrayItems.add((Integer) item); // Once the loop is left, if 'duplicate' is still false, item is added
+				}
+			}
+
 		}
-		Object[] result = arrayItems.toArray(); // Convert the list to an array and store it as an Object[]
+		
+		array = null; // Prepare array argument for garbage collection
+		int[] intArray = new int[arrayItems.size()]; // Create int[] with size equal to the size of arrayItems ArrayList
+		
+		// Iterate through the ArrayList and store each Integer value in the int[]
+		for ( int i = 0; i < intArray.length; i++ ) {
+			intArray[i] = arrayItems.get(i);
+		}
+		
 		arrayItems = null; // Set the arrayItems list to null for garbage collection
 		
-		return result; // Return the result to the main method
+		return intArray; // Return the result to the main method
 	}
 
 	/* 
@@ -40,11 +81,11 @@ public class Problem1_31Jan2022 {
 	 */
 	public static void main(String[] args) {
 //		Object[] array = {1, 2, "a", "b"}; // First array option
-		Object[] array = {1, "a", "b", 0, 15}; // Second array option
-//		Object[] array = {1, 2, "aasf", "1", "123", 123}; // Third array option
+//		Object[] array = {1, "a", "b", 0, 15}; // Second array option
+		Object[] array = {1, 2, "aasf", "1", "123", 123, 2}; // Third array option
 		
 		
-		Object[] result = getResult(array); // Call getResult with an array as the argument and store the results as an Object[]
+		int[] result = getResult(array); // Call getResult with an array as the argument and store the results as an Object[]
 		System.out.println(Arrays.toString(result)); // Print the newly created array as a string.
 	}
 
